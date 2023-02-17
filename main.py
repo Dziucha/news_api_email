a29122b32bc1a7381081a6ef4649cc58dcf7de73
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-17 12:07:25 +0100

    access news titles and descriptions

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ url = "https://newsapi.org/v2/everything?q=tesla&from=2023-01-17&sortBy=" \
 api_key = "e79aef38d6a14ade8d8a3d9eea769861"
 
 request = requests.get(url)
-content = request.text
+content = request.json()
 
-print(content)
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
