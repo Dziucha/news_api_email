70d80c637c5344e3ccedbe7a08663db0ccd5cc68
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-20 16:12:03 +0100

    send e-mail function execution corrected. Avoided None type string append and corrected text to forbid unicode signs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
+from functions import send_email
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-01-17&sortBy=" \
+url = "https://newsapi.org/v2/everything?q=tesla&from=2023-01-23&sortBy=" \
       "publishedAt&apiKey=e79aef38d6a14ade8d8a3d9eea769861"
 
 api_key = "e79aef38d6a14ade8d8a3d9eea769861"
@@ -8,6 +9,20 @@ api_key = "e79aef38d6a14ade8d8a3d9eea769861"
 request = requests.get(url)
 content = request.json()
 
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    try:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+    except TypeError:
+        pass
+
+body = body.encode("ascii", "ignore")
+
+email_message = f"""\
+Subject:Tesla news
+
+{body}
+"""
+
+send_email(email_message)
