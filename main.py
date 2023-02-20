dfd7fc1a4df633ca7d2ad2f5957d9059ec802fcc
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-20 16:25:48 +0100

    universal topic, add links, specified language, limit news to 20

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 from functions import send_email
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-01-23&sortBy=" \
-      "publishedAt&apiKey=e79aef38d6a14ade8d8a3d9eea769861"
+topic = "tesla"
+
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-01-23" \
+      "&sortBy=publishedAt&apiKey=e79aef38d6a14ade8d8a3d9eea769861&language=en"
 
 api_key = "e79aef38d6a14ade8d8a3d9eea769861"
 
@@ -11,16 +13,17 @@ content = request.json()
 
 body = ""
 
-for article in content["articles"]:
+for article in content["articles"][:20]:
     try:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = body + article["title"] + "\n" + article["description"] + "\n" \
+               + article["url"] + 2*"\n"
     except TypeError:
         pass
 
 body = body.encode("ascii", "ignore")
 
 email_message = f"""\
-Subject:Tesla news
+Subject:Today's news
 
 {body}
 """
